6cd8499deda4448ae15d727f3bbcb2ccd35b4204
Sudipto Saha <sudiptosaha5002@gmail.com>
2026-05-24 03:40:13 +0530

    new program added

---
+++ WAP in C to enter 10 array elements and showAll factorial value of each number.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,arr[10];
+	long int f;
+	printf("enter 10 elements into an Array:");
+	for(i=0; i<10; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0; i<10; i++)
+	{
+		f=1;
+		for(j=1; j<=arr[i]; j++)
+		{
+			f=f*j;
+		}
+		printf("%d!=%ld\n",arr[i],f);
+	}
+}
